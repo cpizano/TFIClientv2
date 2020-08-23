0a70a808d9d7cf45da335bdeb4789df0b89bec7b
carlos pizano <carlos.pizano@gmail.com>
2020-08-23 00:52:07 -0700

    update to 20.8.23a
    Still compatible with the 20.8.16 server.  Main changes
    - updated unity version (previous commit)
    - saves player name and server address

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -10,12 +10,39 @@ public class LobbyManager : MonoBehaviour
     public TMP_InputField userNameField;
     public TMP_InputField serverAddress;
 
+    public void Start()
+    {
+        userNameField.text = LoadPlayerName();
+        serverAddress.text = LoadServerAddress();
+    }
+
     public void ConnectToServer()
     {
-        //startMenu.SetActive(false);
-        //userNameField.interactable = false;
+        userNameField.interactable = false;
+        serverAddress.interactable = false;
+        SavePlayerName(userNameField.text);
+        SaveServerAddress(serverAddress.text);
 
         Client.instance.ConnectToServer(userNameField.text, serverAddress.text);
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
+
+    private void SavePlayerName(string name)
+    {
+        PlayerPrefs.SetString("playername", name);
+    }
+    private void SaveServerAddress(string name)
+    {
+        PlayerPrefs.SetString("serveraddr", name);
+    }
+
+    private string LoadPlayerName()
+    {
+        return PlayerPrefs.GetString("playername", "your-name-here");
+    }
+    private string LoadServerAddress()
+    {
+        return PlayerPrefs.GetString("serveraddr", "127.0.0.1");
+    }
+
 }
