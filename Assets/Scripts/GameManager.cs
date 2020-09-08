070ef5e3c9b48119882063a39cdc6547eeda2417
carlos pizano <carlos.pizano@gmail.com>
2020-09-08 00:25:15 -0700

    First step towards scripted tiles
    Gamemanager has now a reference to the first tilemap and now we can
    place tiles from the loaded resources. Not clear whatsoever what is the mapping
    from index to name.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class GameManager : MonoBehaviour
     public GameObject localPlayerPrefab;
     public GameObject playerPrefab;
     public Canvas canvas;
+    public Tilemap tilemap;
+
+    private Tile[] tiles;
 
     private void Awake()
     {
@@ -24,6 +28,11 @@ public class GameManager : MonoBehaviour
             Debug.Log("GameManager already exists, destroying object!");
             Destroy(this);
         }
+    }
+
+    private void Start()
+    {
+        tiles = Resources.FindObjectsOfTypeAll<Tile>();
     }
 
     public void SpawnPlayer(int _id, string _username, Vector3 _position, Quaternion _rotation)
@@ -51,6 +60,17 @@ public class GameManager : MonoBehaviour
         _pm.Init(_id, _username, label, _camera, _local);
 
         players.Add(_id, _pm);
+
+        var cell = tilemap.layoutGrid.WorldToCell(_position);
+        SetTiles(_id, cell.x, cell.y, 1);
+    }
+
+    public void SetTiles(int index, int x, int y, int count)
+    {
+        for (int ix = 0; ix < count; ++ix)
+        {
+            tilemap.SetTile(new Vector3Int(x + ix, y, 0), tiles[index]);
+        }
     }
 
     internal void DeSpawnPlayer(int _id, int reason)
